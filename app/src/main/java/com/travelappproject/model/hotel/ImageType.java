37033cf5f770bb09ac91f156f2c1b42a41246950
hungmai2411 <maiphamquochung@gmail.com>
2022-05-20 23:45:52 +0700

    Create model hotel + room

---
+++ app/src/main/java/com/travelappproject/model/hotel/ImageType.java
@@ -0,0 +1,25 @@
+package com.travelappproject.model.hotel;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.io.IOException;
+
+public enum ImageType {
+    HOTEL_IMAGE;
+
+    @JsonValue
+    public String toValue() {
+        switch (this) {
+            case HOTEL_IMAGE:
+                return "hotel_image";
+        }
+        return null;
+    }
+
+    @JsonCreator
+    public static ImageType forValue(String value) throws IOException {
+        if (value.equals("hotel_image")) return HOTEL_IMAGE;
+        throw new IOException("Cannot deserialize ImageType");
+    }
+}
