0ee4e925e1e6bb9f46b713a1c735372673df8264
hungmai2411 <maiphamquochung@gmail.com>
2022-05-26 15:41:53 +0700

    fix home fragment

---
+++ app/src/main/java/com/travelappproject/Constants.java
@@ -0,0 +1,10 @@
+package com.travelappproject;
+
+public class Constants {
+    private static final String PACKAGE_NAME ="com.travelappproject";
+    static public final String RESULT_DATA_KEY = PACKAGE_NAME+".RESULT_DATA_KEY";
+    static public String RECEIVER = PACKAGE_NAME + ".RECEIVER";
+    static public final String LOCATION_DATA_EXTRA= PACKAGE_NAME+ ".LOCATION_DATA_EXTRA";
+    static public final int SUCCESS_RESULT=1;
+    static public final int FAILURE_RESULT=0;
+}
