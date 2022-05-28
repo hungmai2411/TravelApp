64973efe31a315b00f099187419108b312557cba
hungmai2411 <maiphamquochung@gmail.com>
2022-05-28 11:43:45 +0700

    add fragment favorite

--- app/src/main/java/com/travelappproject/fragments/HomeFragment.java
+++ app/src/main/java/com/travelappproject/fragments/HomeFragment.java
@@ -1,26 +1,33 @@
 package com.travelappproject.fragments;
 
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.facebook.shimmer.ShimmerFrameLayout;
 import com.google.firebase.auth.FirebaseAuth;
+import com.travelappproject.LanguageManager;
 import com.travelappproject.activities.ChooseLocationActivity;
 import com.travelappproject.R;
 import com.travelappproject.activities.HotelDetailActivity;
@@ -47,6 +54,8 @@ public class HomeFragment extends Fragment {
     LinearLayout btnChooseLocation;
     List<Hotel> listHotHotel;
     ImageButton btnSearch;
+    LanguageManager languageManager;
+    ShimmerFrameLayout shimmerFrameLayout;
 
     public HomeFragment() {
     }
@@ -67,8 +76,13 @@ public class HomeFragment extends Fragment {
             state = getArguments().getString("state");
         }
 
+        languageManager = new LanguageManager(getContext());
+        languageManager.updateResource("vi");
+        getFragmentManager().beginTransaction().detach(HomeFragment.this).attach(HomeFragment.this).commit();
+
         hotelViewModel = new ViewModelProvider(getActivity()).get(HotelViewModel.class);
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -80,16 +94,22 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        shimmerFrameLayout = view.findViewById(R.id.shimmer);
+        shimmerFrameLayout.startShimmer();
+
         listHotHotel = new ArrayList<>();
         txtCurrentLocation = view.findViewById(R.id.txtCurrentLocation);
         txtCurrentLocation.setText(state);
         rcvHotHotel = view.findViewById(R.id.rcvHotHotel);
+        rcvHotHotel.setHasFixedSize(true);
+
         rcvNewHotel = view.findViewById(R.id.rcvNewHotel);
         btnChooseLocation = view.findViewById(R.id.btnChooseLocation);
         btnSearch = view.findViewById(R.id.btnSearch);
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false);
         rcvHotHotel.setLayoutManager(linearLayoutManager);
+
         hotHotelAdapter = new HotelAdapter(getContext(), new HotelAdapter.IClickItemListener() {
             @Override
             public void onClickItem(Hotel hotel) {
@@ -101,7 +121,8 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        hotelViewModel.getList((String) txtCurrentLocation.getText());
+        hotHotelAdapter.setData(listHotHotel);
+        //rcvHotHotel.setAdapter(hotHotelAdapter);
         observedHotels();
 
         LinearLayoutManager linearLayoutManager1 = new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false);
@@ -110,7 +131,6 @@ public class HomeFragment extends Fragment {
 
         mListLocation.add("Hà Nội");
         mListLocation.add("Đà Nẵng");
-        mListLocation.add("Hội An");
         mListLocation.add("Sài Gòn");
         mListLocation.add("Phú Quốc");
         mListLocation.add("Nha Trang");
@@ -120,7 +140,6 @@ public class HomeFragment extends Fragment {
 
         mListImages.add(R.drawable.hn_thumbnail);
         mListImages.add(R.drawable.dn_thumbnail);
-        mListImages.add(R.drawable.ha_thumbnail);
         mListImages.add(R.drawable.hcm_thumbnail);
         mListImages.add(R.drawable.pq_thumbnail);
         mListImages.add(R.drawable.nt_thumbnail);
@@ -132,6 +151,18 @@ public class HomeFragment extends Fragment {
                 if (destination == "Sài Gòn") {
                     Intent intent = new Intent(getContext(), ListHotelActivity.class);
                     intent.putExtra("destination", "TP Hồ Chí Minh");
+                    startActivity(intent);
+                } else if (destination == "Phú Quốc") {
+                    Intent intent = new Intent(getContext(), ListHotelActivity.class);
+                    intent.putExtra("destination", "Kiên Giang");
+                    startActivity(intent);
+                } else if (destination == "Nha Trang") {
+                    Intent intent = new Intent(getContext(), ListHotelActivity.class);
+                    intent.putExtra("destination", "Khánh Hòa");
+                    startActivity(intent);
+                }else if (destination == "Đà Lạt") {
+                    Intent intent = new Intent(getContext(), ListHotelActivity.class);
+                    intent.putExtra("destination", "Lâm Đồng");
                     startActivity(intent);
                 } else {
                     Intent intent = new Intent(getContext(), ListHotelActivity.class);
@@ -155,20 +186,9 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 SearchFragment searchFragment = new SearchFragment();
-                //startActivity(new Intent(getContext(), SearchFragment.class));
                 getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.content_container, searchFragment).commit();
             }
         });
-
-    }
-
-    private boolean checkExist(List<Hotel> list, Hotel hotel) {
-        for (Hotel hotel1 : list) {
-            if (hotel1.getName() == hotel.getName())
-                return true;
-        }
-
-        return false;
     }
 
     private void observedHotels() {
@@ -176,13 +196,15 @@ public class HomeFragment extends Fragment {
             @Override
             public void onChanged(List<Hotel> listHotel) {
                 for (Hotel hotel : listHotel) {
-                    if (hotel.getStarRate() >= 4 && checkExist(listHotHotel, hotel) == false) {
+                    if (hotel.getStarRate() >= 4) {
                         listHotHotel.add(hotel);
                     }
-
                 }
 
-                hotHotelAdapter.setData(listHotHotel);
+                shimmerFrameLayout.stopShimmer();
+                shimmerFrameLayout.setVisibility(View.GONE);
+                rcvHotHotel.setVisibility(View.VISIBLE);
+                hotHotelAdapter.notifyDataSetChanged();
                 rcvHotHotel.setAdapter(hotHotelAdapter);
             }
         });
