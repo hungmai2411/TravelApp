077c4ed51835ffaff16b533dc13c929e62ec8b7d
nguyenduylinh <duy0000linh0000@gmail.com>
2022-05-30 09:22:33 +0700

    Fix Payment Method

--- app/src/main/java/com/travelappproject/helperforzalopay/AppInfo.java
+++ app/src/main/java/com/travelappproject/helperforzalopay/AppInfo.java
@@ -1,9 +1,7 @@
 package com.travelappproject.helperforzalopay;
 
 public class AppInfo {
-    public static final int APP_ID = 554;
-    public static final String MAC_KEY = "8NdU5pG5R2spGHGhyO99HN1OhD8IQJBn";
+    public static final int APP_ID = 2554;
+    public static final String MAC_KEY = "sdngKKJmqEMzvh5QQcdD2A9XBSKUNaYn";
     public static final String URL_CREATE_ORDER = "https://sb-openapi.zalopay.vn/v2/create";
-    //public static final String URL_CREATE_ORDER = "https://sandbox.zalopay.com.vn/v001/tpe/createorder";
-
 }
